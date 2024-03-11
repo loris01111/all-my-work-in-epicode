681abec5b9424aa6d561d8c5620c5823a49b6424
loris01111 <boveloris@gmail.com>
2024-03-11 21:52:34 +0100

    update

--- M4_W1D4_SpotifyAPI/HW2/script.js
+++ M4_W1D4_SpotifyAPI/HW2/script.js
@@ -45,11 +45,13 @@
 // }
 
 const eminemCardContainer = document.getElementById("eminem");
+const metallicaCardContainer = document.getElementById("metallica");
+const queenCardContainer = document.getElementById("queen");
 
 
 const urlEminem = "https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem";
-const url1 = "https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica";
-const url2 = "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen";
+const urlMetallica = "https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica";
+const urlQueen = "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen";
 
 const getCardsEminem = async () => {
     try {
@@ -63,6 +65,29 @@ const getCardsEminem = async () => {
     }
 }
 
+const getCardsMetallica = async () => {
+    try {
+        const response = await fetch(urlMetallica);
+        const data = await response.json();
+
+        console.log(data);
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getCardsQueen = async () => {
+    try {
+        const response = await fetch(urlQueen);
+        const data = await response.json();
+
+        console.log(data);
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 // const songs = getCardsEminem().then(res => {
 //     res.map(song => createCard(song))
@@ -88,17 +113,61 @@ const createCard = (song) => {
     eminemCardContainer.appendChild(card);
 };
 
+const createCardMetallica = (song) => {
+    const card = document.createElement("div");
+    card.classList.add("card", "mb-5", "w-50");
+
+    const title = document.createElement("h3");
+    title.classList.add("card-title", "text-dark", "text-center");
+    title.innerHTML = song.title;
+
+    const img = document.createElement("img");
+    img.src = song.album.cover;
+    img.classList.add("card-img-top", "img-fluid", "w-100", "h-100");
+
+    card.append(title,img);
+    metallicaCardContainer.appendChild(card);
+}
+
+const createCardQueen =  (song) => {
+    const card = document.createElement("div");
+    card.classList.add("card", "mb-5", "w-50");
+
+    const title = document.createElement("h3");
+    title.classList.add("card-title", "text-dark", "text-center");
+    title.innerHTML = song.title;
+
+    const img = document.createElement("img");
+    img.src = song.album.cover;
+    img.classList.add("card-img-top", "img-fluid", "w-100", "h-100");
+
+    card.append(title,img);
+    queenCardContainer.appendChild(card);
+}
 
 const search =  () => {
     const buttonToSearch = document.querySelector("#button-search");
     const inputSearchValue = document.querySelector("#searchField");
     buttonToSearch.addEventListener("click", () => {
-        getCardsEminem().then(res => {
-            console.log(res.map);
-            res.map(song => createCard(song))
-        })
+        
         if (inputSearchValue.value.indexOf("eminem")!== -1) {
+            getCardsEminem().then(res => {
+                console.log(res.map);
+                res.map(song => createCard(song))
+            })
             eminemCardContainer.setAttribute("class", "display: block");
+        }if (inputSearchValue.value.indexOf("metallica")!== -1) {
+            getCardsMetallica().then(res => {
+                console.log(res.map);
+                res.map(song => createCardMetallica(song))
+            })
+            metallicaCardContainer.setAttribute("class", "display: block");
+        }if (inputSearchValue.value.indexOf("queen")!== -1) {
+            getCardsQueen().then(res => {
+                console.log(res.map);
+                res.map(song => createCardQueen(song))
+            })
+            queenCardContainer.setAttribute("class", "display: block");
         }else {
             console.log("non c'è nulla");
         }
