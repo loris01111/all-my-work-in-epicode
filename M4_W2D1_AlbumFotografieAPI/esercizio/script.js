87e0228a039293aeaf7e91a498a484eaa34325c6
loris01111 <boveloris@gmail.com>
2024-03-14 18:06:26 +0100

    update

--- M4_W2D1_AlbumFotografieAPI/esercizio/script.js
+++ M4_W2D1_AlbumFotografieAPI/esercizio/script.js
@@ -10,22 +10,9 @@ const fetchPhoto = (query) => {
         .then(data => {
             const containerCards = document.getElementById("containerCards");
             containerCards.innerHTML = data.photos.map((photo) => {
-                return ` <div class='col col-4'> <div class="card mb-4 shadow-sm">
-                <img src='${photo.src.large}' />
-
-                <div class="card-body">
-                  
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    
-                    <small class="text-muted">${photo.id}</small>
-                  </div>
-                </div>
-              </div> </div>`
+                return `<img src='${photo.src.medium}' />`
             })
         })
-
         .catch((err) => console.log(err));
 }
 
